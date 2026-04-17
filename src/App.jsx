d2d4df5827b49623ac295445c0439aa2474f88d7
Harjot KAUR <harjotkaur6677@gmail.com>
2026-04-17 13:58:21 +0530

    day8

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome Jot Sharma!</h1>
+      <p>Your age is 22</p>
+    </div>
+  );
+};
+
+export default App;   // Must have default export for main.jsx
